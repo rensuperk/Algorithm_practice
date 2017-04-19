30537b53ee3a9721481aa7c9bc140972095aab41
renkai <304078213@qq.com>
2017-04-19 17:50:45 +0800

    push

--- Readme.md
+++ Readme.md
@@ -4,6 +4,7 @@
 ### [476. Number Complement](md/476.NumberComplement.md)
 ### [339. Nested List Weight Sum](md/339.NestedListWeightSum.md)
 ### [557. Reverse Words in a String III](md/557.ReverseWordsinaStringIII.md)
+### [500. Keyboard Row](md/500.KeyboardRow.md)
 
 
 
---
+++ src/main/java/KeyboardRow.java
@@ -0,0 +1,56 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**判断是否在一行
+ * Created by renkai on 2017/4/19.
+ */
+public class KeyboardRow {
+    public String[] solution(String[] input){
+        List<String> list = new ArrayList<String>();
+        String u ="qwertyuiop";
+        String m ="asdfghjkl";
+        String d ="zxcvbnm";
+        for (int i = 0; i < input.length; i++) {
+            int before = 0;
+            int next = 0;
+            boolean bflag = true;
+            char[] chars = input[i].toLowerCase().toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+                if(contain(u,chars[j])){
+                    next =0;
+                }else if(contain(m,chars[j])){
+                    next =1;
+                }else if(contain(d,chars[j])){
+                    next =2;
+                }
+                if(j > 0 && before != next){
+                    bflag = false;
+                    break;
+                }
+                before = next;
+            }
+            if(bflag){
+                list.add(input[i]);
+            }
+        }
+        return list.toArray(new String[list.size()]);
+    }
+    boolean contain(String s,char c){
+        for (char c1 : s.toCharArray()) {
+            if(c1 == c){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        KeyboardRow keyboardRow = new KeyboardRow();
+        String[] solution = keyboardRow.solution(new String[]{"asd", "qwe"});
+        for (String s : solution) {
+
+            System.out.println(s);
+        }
+    }
+}
